2d72147ea2841834eefa68a409e04a49e552932a
Noshaid Ali <noshaid.ali@khaleef.com>
2021-04-06 11:25:40 +0500

    Add main screens of app

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { StyleSheet, Text, View, Button, FlatList } from "react-native";
+
+import { CATEGORIES } from "../data/dummy-data";
+
+const renderGridItem = (itemData) => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
+
+const CategoriesScreen = (props) => {
+  return (
+    <FlatList
+      keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  gridItem: {
+      flex: 1,
+      margin: 15,
+      height: 150
+    }
+});
+
+export default CategoriesScreen;
+
+//props.navigation.navigate({ routeName: "CategoryMeals" }); //don't push same screen
+//props.navigation.navigate('CategoryMeals') //don't push same screen
+//props.navigation.push('CategoryMeals') //push same screen
+//props.navigation.replace('CategoryMeals') //replace the screen, can't go back
